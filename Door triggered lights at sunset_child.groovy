8bfa67b7f59b872d7e105d71b781e2227fc72754
scottdog123 <curt_scott@yahoo.com>
2018-11-02 14:29:35 -0700

    Add files via upload

---
+++ Door triggered lights at sunset_child.groovy
@@ -0,0 +1,90 @@
+/**
+ *  Door triggered lights at sunset
+ *
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "DoorTriggeredLightsSunsetChild",
+    namespace: "scottdog123",
+    author: "Curt Scott",
+    parent: "scottdog123:DoorTriggeredLightsSunsetParent",
+    description: "Turns on lights when door opens after sunset for x minutes",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/rise-and-shine.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/rise-and-shine@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/rise-and-shine@2x.png")
+
+preferences {
+    section("Lights") {
+        input "switches", "capability.switch", title: "Which light to turn on?", multiple: true
+        input "disable_switch", "capability.switch", title: "Disable Switch", required: false
+        input "doorSensor", "capability.contactSensor", title: "Which Door to trigger on?", multiple: true
+//    input "motionSensor", "capability.motionSensor", title: "Which Motion Sensor?", required: false
+    input "motionSensor", "capability.switch", title: "Which Motion Sensor?", required: false
+    input "Delay", "number", title: "Delay(min) before turning OFF light"
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    subscribe(doorSensor, "contact.open", motionOrDoorEvent)
+//    subscribe(motionSensor, "motion.active", motionOrDoorEvent)
+  subscribe(motionSensor, "switch.on", motionOrDoorEvent)
+  subscribe(location, "sunriseTime", sunRiseHandler)
+  subscribe(location, "sunsetTime", sunSetHandler)
+    def SunriseAndSunset = getSunriseAndSunset()
+    if (now() >= SunriseAndSunset.sunrise.time && now() < SunriseAndSunset.sunset.time) {
+      state.nightTime = 0
+      log.debug "start daymode"
+  }
+    else {
+      state.nightTime = 1
+       log.debug "start nightmode"
+  }    
+}
+
+def lightOffHandler() {
+    // log.debug "turning off lights"
+    switches.off()
+}
+
+def motionOrDoorEvent(evt) {
+  if (disable_switch && disable_switch.currentValue("switch") == "on") {
+      log.debug "disable_switch on"
+        return
+    }
+    if (state.nightTime == 1) {
+    switches.on()
+      // log.debug "door open"
+    // sendPush("Door open: ${evt.name}")
+      runIn(Delay*60, lightOffHandler, [overwrite: true])
+  }
+}
+
+def sunRiseHandler(evt) {
+  state.nightTime = 0
+  // log.debug "Day Time"
+}
+
+def sunSetHandler(evt) {
+  state.nightTime = 1
+    // log.debug "Night Time"
+}
---
+++ Door triggered lights at sunset_parent.groovy
@@ -0,0 +1,39 @@
+definition(
+    name: "DoorTriggeredLightsSunsetParent",
+    namespace: "scottdog123",
+    author: "Curt Scott",
+    description: "Turns on lights when door opens after sunset for x minutes - parent",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    // The parent app preferences are pretty simple: just use the app input for the child app.
+    page(name: "mainPage", title: "Simple Automations", install: true, uninstall: true,submitOnChange: true) {
+        section {
+            app(name: "childApps", appName: "DoorTriggeredLightsSunsetChild", namespace: "scottdog123", title: "Create New Automation", multiple: true)
+            }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // nothing needed here, since the child apps will handle preferences/subscriptions
+    // this just logs some messages for demo/information purposes
+    log.debug "there are ${childApps.size()} child smartapps"
+    childApps.each {child ->
+        log.debug "child app: ${child.label}"
+    }
+}
